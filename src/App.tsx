527cb486e429a29680885cdbc67552441c54eb38
jonsumisu628 <ten_1106@icloud.com>
2021-02-28 22:26:42 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 import Footer from "./components/atoms/footer";
 import title from "./components/img/text.png";
 import image from './components/img/img.jpg';
-import Text from "./components/atoms/text";
 
 function App() {
   return (
@@ -12,7 +11,22 @@ function App() {
       <Body>
         <Main>
           <Title src={title} />
-          <Text />
+          <Text>
+            <Heading>About</Heading>
+            <Description>
+              NicName:John.Smith<br></br>BirthDay:1999/6/28<br></br>Location：Tokyo<br></br>
+                東京の専門学校に通っています。<br></br>現在就職活動中...
+            </Description>
+            <Heading>Study</Heading>
+            <Description>
+              JavaScript / HTML / CSS / React / Gatuby <br></br>
+                Vue / Svelte / Linux / Docker / Vagurant
+            </Description>
+            <Heading>Pastime</Heading>
+            <Description>
+              アニメやゲームが好きです。バスケやスノースポーツもやります。専門学校に入学してからは筋トレも初めました。
+            </Description>
+          </Text>
           <Img src={image} />
         </Main>
         <Footer />
@@ -22,14 +36,40 @@ function App() {
 }
 
 const Body = styled.div`
-  display:flex;
-  flex-direction:column;
-  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  @media (min-height: 630px) {
+    justify-content: center;
+  }
 `;
 
 const Main = styled.div`
-  margin: 60px 60px 58px 60px;
+  margin: 58px 60px 58px 60px;
   display: flex;
+  justify-content: center;
+  @media (min-width: 933px) {
+    justify-content: space-around;
+  }
+`;
+
+const Text = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 560px;
+`;
+
+const Heading = styled.p`
+  margin: 12px 0px 12px 0px;
+  font-family: "游ゴシック体", YuGothic, "游ゴシック", "Yu Gothic", sans-serif;
+  font-weight: bold;
+`;
+
+const Description = styled.p`
+  margin: 12px 0px 12px 0px;
+  font-family: "游ゴシック体", YuGothic, "游ゴシック", "Yu Gothic", sans-serif;
+  font-size: 21px;
+  line-height: 30px;
 `;
 
 const Title = styled.img`
